0b52804ce27b982898ac5f50529c0d1374d3319c
philipeneves <49224281+philipeneves@users.noreply.github.com>
2020-02-12 13:26:00 -0300

    Create retornaLucroDef.py

---
+++ retornaLucroDef.py
@@ -0,0 +1,16 @@
+def retornaLucro():
+  input= [7,6,4,3,1]
+  compra = 0
+  venda = 0
+  for i in range(len(input)):
+    if (input[i] == min(input)):    
+      compra = input[i];    
+    elif (i + 1 < len(input)):      
+      if (input[i] > input[i + 1]):      
+        if (compra > 0):   
+          venda = input[i];
+
+  if (venda - compra > 0):
+    print(venda - compra);
+  else:
+    print(0);
